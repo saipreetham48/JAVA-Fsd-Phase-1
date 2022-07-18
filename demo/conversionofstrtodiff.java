14993b9448af44509f7b0045c5bad64b4b212dfd
unknown <sreddy10500@gmail.com>
2022-07-18 18:19:02 +0530

    typecasting

---
+++ demo/conversionofstrtodiff.java
@@ -0,0 +1,25 @@
+package strtoother;
+
+public class conversionofstrtodiff {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+        String str = "123";
+	    int a = Integer.parseInt(str);
+          System.out.println("str to int:"+a);
+          float b =Float.parseFloat(str);
+          System.out.println("str to float" +b);
+          double c =Double.parseDouble(str);
+          System.out.println("str to double " +c);
+          byte d = Byte.parseByte(str);
+          System.out.println("str to byte " +d);
+          short e =Short.parseShort(str);
+          System.out.println("str to short " +e);
+          long f = Long.parseLong(str);
+          System.out.println("str to long " +f);
+  
+	}
+
+}
