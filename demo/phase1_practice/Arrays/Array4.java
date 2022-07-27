68bbdb9e4a0337fd4c9f1483c4462177062e9833
unknown <sreddy10500@gmail.com>
2022-07-27 09:04:39 +0530

    arrays

---
+++ demo/phase1_practice/Arrays/Array4.java
@@ -0,0 +1,99 @@
+package Arrays;
+import java.io.*;
+public class Array4 {
+	Node head; // head of list 
+	static class Node 
+{ 
+    		int data; 
+    		Node next; 
+    		Node(int d) 
+    		{ 
+        			data = d; 
+        			next = null; 
+    		} 
+}
+
+//Method to insert a new node 
+ 	public static Array4 insert(Array4 list, int data) 
+ 	{ 
+     		// Create a new node with given data 
+     		Node new_node = new Node(data); 
+     		new_node.next = null; 
+		// If the Linked List is empty, then make the new node as head 
+     		if (list.head == null) 
+{ 
+         			list.head = new_node; 
+     		} 
+     		else 
+{ 
+         			// Else traverse till the last node and insert the new_node there 
+         			Node last = list.head; 
+         			while (last.next != null) 
+{ 
+             			last = last.next; 
+         			} 
+			// Insert the new_node at last node 
+         			last.next = new_node; 
+     		} 
+     		return list; 
+ 	} 
+	public static void printList(Array4 list) 
+ 	{	 
+     		Node currNode = list.head; 
+     		System.out.print("LinkedList: "); 
+     		// Traverse through the LinkedList 
+     		while (currNode != null) 
+{ 
+         			// Print the data at current node 
+         			System.out.print(currNode.data + " "); 
+         			// Go to next node 
+         			currNode = currNode.next; 
+     		} 
+     		System.out.println(); 
+ 	} 
+ 	// Method to delete a node in the LinkedList by KEY
+	public static Array4 deleteByKey(Array4 list, int key) 
+	{ 
+    		// Store head node 
+    		Node currNode = list.head, prev = null; 
+    		if (currNode != null && currNode.data == key) 
+{ 
+        			list.head = currNode.next; // Changed head 
+        			System.out.println(key + " found and deleted"); 
+        			return list; 
+    		} 
+    		while (currNode != null && currNode.data != key) 
+{ 
+        			prev = currNode; 
+        			currNode = currNode.next; 
+    		} 
+    		if (currNode != null) 
+{ 
+        			prev.next = currNode.next; 
+        			System.out.println(key + " found and deleted"); 
+    		} 
+    		if (currNode == null) 
+{ 
+        			System.out.println(key + " not found"); 
+    		} 
+    		return list; 
+	} 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Array4 list=new Array4();
+		list = insert(list, 1); 
+		list = insert(list, 2); 
+		list = insert(list, 3); 
+		list = insert(list, 4); 
+		list = insert(list, 5); 
+		list = insert(list, 6); 
+		list = insert(list, 7); 
+		list = insert(list, 8); 
+		printList(list);
+		deleteByKey(list, 1);
+		printList(list); 
+		deleteByKey(list, 4); 
+		printList(list); 
+		deleteByKey(list, 10);
+		printList(list);
+}}
---
+++ demo/phase1_practice/Arrays/Queuework.java
@@ -0,0 +1,22 @@
+package prac;
+
+import java.util.*;
+
+public class Queuework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<String> locationsQueue = new LinkedList<>();
+		locationsQueue.add("Kolkata");
+		locationsQueue.add("Patna");
+		locationsQueue.add("Delhi");
+		locationsQueue.add("Gurgaon");
+		locationsQueue.add("Noida");
+		System.out.println("Queue is : " + locationsQueue);
+		System.out.println("Head of Queue : " + locationsQueue.peek());
+		locationsQueue.remove();
+		System.out.println("After removing Head of Queue : " + locationsQueue);
+		System.out.println("Size of Queue : " + locationsQueue.size());
+	}
+
+}
